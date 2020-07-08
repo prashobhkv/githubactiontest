c851d5f4101298cbe38076a4eec4dd58c588d568
prashobh <prashobhkv@gmail.com>
2020-07-08 20:08:09 +0530

    second deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First APP Deployed Sucessfully
+          Second APP Deployed Sucessfully
         </p>
         <a
           className="App-link"
